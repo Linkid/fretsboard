b8d465e5d80bf235ace366c16b38bd9b8ceef58a
François Magimel <magimel.francois@gmail.com>
2017-09-02 22:36:46 +0200

    Display scores for each song

--- scoreboard/urls.py
+++ scoreboard/urls.py
@@ -6,6 +6,7 @@ from scoreboard import views
 urlpatterns = [
     # songs
     url(r'^songs/$', views.Songs.as_view(), name='songs'),
+    url(r'^songs/(?P<song_id>.+)/$', views.song, name='song'),
     # players
     url(r'^players/$', views.Players.as_view(), name='players'),
 ]
--- scoreboard/views.py
+++ scoreboard/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 from django.views import generic
 
 from scoreboard.models import Player
@@ -9,6 +11,14 @@ class Songs(generic.ListView):
     context_object_name = 'songs'
     template_name = 'scoreboard/songs.html'
     paginate_by = 20
+
+
+def song(request, song_id):
+    """
+    Song scores
+    """
+    song = get_object_or_404(Song, slug=song_id)
+    return render(request, 'scoreboard/song.html', { 'song': song })
 
 
 class Players(generic.ListView):

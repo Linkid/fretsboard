e5b091c9745e8392429d26df3ff6458442673cf2
François Magimel <magimel.francois@gmail.com>
2017-10-29 13:48:53 +0100

    Move static dirs to apps dir
    This will let us adding STATIC_ROOT to settings and use the
    collectstatic command.

--- fretsboard/settings.py
+++ fretsboard/settings.py
@@ -108,11 +108,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'fretsboard', 'static'),
 )

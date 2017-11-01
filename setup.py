609e9d1dec0ed6701dc28340821e5407e1a260d1
François Magimel <magimel.francois@gmail.com>
2017-11-01 16:00:47 +0100

    Add a setup file

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+version = "1.0"
+
+setup(
+    name="Fretsboard",
+    version=version,
+    author="François Magimel",
+    author_email="francois.magimel@perdu.fr",
+    description="Fretsboard is a web scoreboard for the rhythm game Frets On Fire and similar",
+    long_description=open("README.md", 'rb').read().decode('utf-8'),
+    license="GPL License",
+    url="https://github.com/Linkid/fretsboard",
+    packages=['fretsboard', 'scoreboard'],
+    platforms='any',
+    zip_safe=True,
+    install_requires=[
+        "django >= 1.11",
+        "django-bootstrap3 >= 9.0",
+    ],
+    dependency_links=[
+        "https://bitbucket.org/jibalamy/cerealizer/get/ffbb928.tar.gz",  # cerealizer 0.8.1
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Games/Entertainment",
+        "Topic :: Internet",
+        "Topic :: Multimedia",
+    ],
+    keywords="scoreboard fofix fretsonfire",
+)
